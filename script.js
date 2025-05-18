0b5a70e30378eb1060a761fa7b7f1496d45c878d
ikauuuu <ikerfernandezmarrero@gmail.com>
2025-05-18 21:32:32 +0100

    testo justificado y moar grande

--- script.js
+++ script.js
@@ -19,3 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
